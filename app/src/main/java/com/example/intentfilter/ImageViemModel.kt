d00f03c32c2dbaae2c08b1708f60fca229af3677
“PMB” <PMB@d.c>
2024-03-07 08:11:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/intentfilter/ImageViemModel.kt
@@ -0,0 +1,15 @@
+package com.example.intentfilter
+
+import android.net.Uri
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ImageViemModel:ViewModel() {
+    var uri : Uri? by mutableStateOf(null)
+        private  set
+    fun updateUri(uri: Uri?){
+        this.uri = uri
+    }
+}
---
+++ app/src/main/java/com/example/intentfilter/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.intentfilter
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
+import com.example.intentfilter.ui.theme.IntentFilterTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<ImageViemModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            IntentFilterTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    viewModel.uri?.let {
+                        AsyncImage(model = viewModel.uri, contentDescription =null )
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        val uri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM, Uri::class.java)
+        } else {
+            intent?.getParcelableExtra(Intent.EXTRA_STREAM)
+
+        }
+        viewModel.updateUri(uri)
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    IntentFilterTheme {
+        Greeting("Android")
+    }
+}
